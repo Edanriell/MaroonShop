142fbca7b448b965902fcb18eda2d78be77c2fd4
Edanriell <Edanriell@gmail.com>
2023-04-14 15:10:53 +0300

    structured project

---
+++ client/src/app/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.scss";
+
+function App() {
+	return (
+		<div>
+			<header>
+				<a href="#" className="text-3xl font-bold underline">
+					Learn React
+				</a>
+				<p className="text-3xl text-cyan-800">Tailwind test</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
