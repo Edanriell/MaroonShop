a4c80f148f1c347838a399539d1a24f36d2d3c11
Edanriell <Edanriell@gmail.com>
2023-04-14 09:55:53 +0300

    added missing modules, added missing configs, fixed tailwind, prettier and eslint

--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    printWidth: 100,
-    tabWidth: 4,
-    singleQuote: false,
-    semi: true,
-    trailingComma: "all",
-    arrowParens: "always",
-    quoteProps: "consistent",
-    endOfLine: "auto",
-    overrides: [
-        {
-            files: "*.{json,yml,md}",
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: "*.{ts,tsx}",
-            options: {
-                parser: "typescript",
-            },
-        },
-    ],
+	printWidth: 100,
+	tabWidth: 4,
+	singleQuote: false,
+	semi: true,
+	trailingComma: "all",
+	arrowParens: "always",
+	quoteProps: "consistent",
+	endOfLine: "auto",
+	useTabs: true,
+	overrides: [
+		{
+			files: "*.{json,yml,md}",
+			options: {
+				tabWidth: 2,
+			},
+		},
+		{
+			files: "*.{ts,tsx}",
+			options: {
+				parser: "typescript",
+			},
+		},
+	],
 };
