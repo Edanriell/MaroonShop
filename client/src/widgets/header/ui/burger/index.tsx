6470da162eabaf0836681b532c8343dee5dfc391
Edanriell <Edanriell@gmail.com>
2023-04-17 22:27:33 +0300

    decomposed burger menu code

--- client/src/widgets/header/ui/burger/index.tsx
+++ client/src/widgets/header/ui/burger/index.tsx
@@ -1,107 +1,41 @@
-import { useState, useRef, useLayoutEffect, FunctionComponent, SVGProps } from "react";
+import { useState, useRef, useLayoutEffect } from "react";
 // Importing useState hook. Importing necessary types for SocialLinks type.
 
-import { Link } from "react-router-dom";
-// Importing Link component.
-
-import { v4 as uuidv4 } from "uuid";
-// Importing unique id generator for keys.
-
 import { gsap } from "gsap";
-// Animation library.
+// Importing animation library.
+
+import BurgerMenu from "../burgerMenu";
+// Importing BurgerMenu component;
 
 import { ReactComponent as Bar } from "./assets/bar.svg";
 //Importing Bar svg.
-import { ReactComponent as Facebook } from "./assets/facebook.svg";
-import { ReactComponent as Instagram } from "./assets/instagram.svg";
-import { ReactComponent as Twitter } from "./assets/twitter.svg";
-// Importing icons for social links. If we import them straight without ReactComponent we will get "links"
-// not an actual SVG (<svg></svg>). In that case we will need to use an img tag with src.
 
 type IsDisplayed = boolean;
 // Type for our isDisplayed state.
 
-type BurgerNavigation = Array<{
-	label: string;
-	url: string;
-	key: string;
-}>;
-// BurgerNavigation type.
-
-type SocialLinks = Array<{
-	label: string;
-	Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
-	iconWidth: string;
-	iconHeight: string;
-	url: string;
-	key: string;
-}>;
-// SocialLinks type.
-
-// TODO FIX links when will be creating corresponding pages.
-const burgerNavigation: BurgerNavigation = [
-	{
-		label: "Каталог",
-		url: "/",
-		key: uuidv4(),
-	},
-	{
-		label: "О нас",
-		url: "/",
-		key: uuidv4(),
-	},
-	{
-		label: "Контакты",
-		url: "/",
-		key: uuidv4(),
-	},
-];
-// mainNavigation contains an array of objects. We will use objects to generate correct NavLinks.
-
-const socialLinks: SocialLinks = [
-	{
-		label: "Наша страничка в Facebook",
-		Icon: Facebook,
-		iconWidth: "w-[1.1rem]",
-		iconHeight: "h-[1.8rem]",
-		url: "/",
-		key: uuidv4(),
-	},
-	{
-		label: "Наша страничка в Instagram",
-		Icon: Instagram,
-		iconWidth: "w-[2rem]",
-		iconHeight: "h-[2rem]",
-		url: "/",
-		key: uuidv4(),
-	},
-	{
-		label: "Наша страничка в Twitter",
-		Icon: Twitter,
-		iconWidth: "w-[2rem]",
-		iconHeight: "h-[1.7rem]",
-		url: "/",
-		key: uuidv4(),
-	},
-];
-
-// TODO Remove all shit code, decompose burger menu to other components BurgerMenuContent and so on
 function Burger() {
 	const [isDisplayed, setIsDisplayed] = useState<IsDisplayed>(false);
 
 	const animation = useRef(null);
+
 	const [ctx] = useState(gsap.context(() => {}, animation));
 
 	useLayoutEffect(() => {
-		gsap.fromTo(
-			animation.current,
-			{ opacity: 0, translateY: -80 },
-			{
-				opacity: 1,
-				translateY: 0,
-				duration: 0.25,
-			},
-		);
+		if (animation.current) {
+			gsap.fromTo(
+				animation.current,
+				{
+					opacity: 0,
+					translateY: -80,
+				},
+				{
+					opacity: 1,
+					translateY: 0,
+					duration: 0.5,
+					ease: "power2.out",
+				},
+			);
+		}
 
 		ctx.add("remove", () => {
 			gsap.fromTo(
@@ -111,6 +45,7 @@ function Burger() {
 					opacity: 0,
 					translateY: -80,
 					duration: 0.25,
+					ease: "power2.out",
 					onComplete: () => {
 						setIsDisplayed(false);
 					},
@@ -119,7 +54,7 @@ function Burger() {
 		});
 
 		return () => ctx.revert();
-	});
+	}, [isDisplayed]);
 
 	function handleDisplayClick() {
 		if (!isDisplayed) {
@@ -143,43 +78,14 @@ function Burger() {
 				<Bar className="w-[2.2rem] h-[0.2rem] text-blue-zodiac md:w-[2.4rem] md:h-[0.3rem]" />
 				<Bar className="w-[2.2rem] h-[0.2rem] text-blue-zodiac md:w-[2.4rem] md:h-[0.3rem]" />
 			</div>
-			{isDisplayed ? (
+			{isDisplayed && (
 				<div
 					ref={animation}
-					className="absolute top-0 left-0 w-full bg-desert-storm shadow-burgerMenu"
+					className="absolute top-0 left-0 w-full bg-desert-storm shadow-burgerMenu box"
 				>
-					<div className="container pt-[11.6rem] pb-[5rem] md:pt-[15.1rem]">
-						<nav aria-label="burger menu navigation">
-							<ul className="flex flex-col gap-y-[3.5rem] md:gap-y-[4rem]">
-								{burgerNavigation.map(({ label, url, key }) => (
-									<li key={key}>
-										<Link
-											to={url}
-											className="font-medium text-sm-28px font-raleway text-blue-zodiac md:text-md-32px"
-										>
-											{label}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</nav>
-						<ul className="flex items-center justify-start mt-[5rem] gap-x-[3rem] md:gap-x-[2.5rem] md:mt-[6rem]">
-							{socialLinks.map(({ label, Icon, iconWidth, iconHeight, url, key }) => (
-								<li key={key}>
-									<Link to={url}>
-										<Icon
-											className={
-												iconWidth + " " + iconHeight + " text-blue-zodiac"
-											}
-										/>
-										<span className="sr-only">{label}</span>
-									</Link>
-								</li>
-							))}
-						</ul>
-					</div>
+					<BurgerMenu />
 				</div>
-			) : null}
+			)}
 		</div>
 	);
 }
