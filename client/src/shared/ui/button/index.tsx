21ac532c9d93428587d2db743be4729ab4b3588d
Edanriell <Edanriell@gmail.com>
2023-04-27 09:10:22 +0300

    finished button hover effect

--- client/src/shared/ui/button/index.tsx
+++ client/src/shared/ui/button/index.tsx
@@ -1,4 +1,9 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+
+import { mouseEnterHoverEffect, mouseLeaveHoverEffect } from "./model";
+
+import styles from "./styles.module.scss";
 
 type ButtonType = "button" | "link";
 
@@ -14,35 +19,89 @@ const buttonClasses = `
 	text-sm-14px-lh-20px border-bombay-400 border-solid 
 	border-[0.1rem] rounded-[0.2rem] pt-[1.3rem] pl-[2.8rem] pb-[1.3rem] pr-[2.8rem]
 	md:text-md-16px-lh-22px pl-[2.7rem] pr-[2.7rem]
-	pointer-events-auto kshczx
+	pointer-events-auto relative overflow-hidden
 `;
 
 function Button({ type, text, link, linkExternal }: Props) {
+	const buttonBoxRef = useRef(null);
+	const buttonTextRef = useRef(null);
+	const buttonRef = useRef(null);
+
+	function handleButtonMouseEnter() {
+		mouseEnterHoverEffect({
+			buttonBoxRef,
+			buttonTextRef,
+			buttonRef,
+		});
+	}
+
+	function handleButtonMouseLeave() {
+		mouseLeaveHoverEffect({
+			buttonBoxRef,
+			buttonTextRef,
+			buttonRef,
+		});
+	}
+
 	if (type === "button") {
 		return (
-			<button className={buttonClasses}>
-				<div className="_styled__StyledButtonFiller-sc-16l8j4-0 hCNHmm"></div>
-				<span className="_styled__StyledButtonText-sc-16l8j4-8 MxKqx">
-					<span className="_styled__StyledButtonTextInnter-sc-16l8j4-9 hUSNqa">
-						{text}
-					</span>
+			<button
+				className={buttonClasses}
+				onMouseEnter={handleButtonMouseEnter}
+				onMouseLeave={handleButtonMouseLeave}
+				ref={buttonRef}
+			>
+				<div className={styles.buttonBox} ref={buttonBoxRef}></div>
+				<span className="relative z-10 block" ref={buttonTextRef}>
+					{text}
 				</span>
 			</button>
 		);
 	} else if (type === "link" && link) {
 		return (
-			<Link to={link} className={buttonClasses}>
-				{text}
+			<Link
+				to={link}
+				className={buttonClasses}
+				onMouseEnter={handleButtonMouseEnter}
+				onMouseLeave={handleButtonMouseLeave}
+				ref={buttonRef}
+			>
+				<div className={styles.buttonBox} ref={buttonBoxRef}></div>
+				<span className="relative z-10 block" ref={buttonTextRef}>
+					{text}
+				</span>
 			</Link>
 		);
 	} else if (type === "link" && linkExternal) {
 		return (
-			<Link to={{ pathname: linkExternal }} target="_blank" className={buttonClasses}>
-				{text}
+			<Link
+				to={{ pathname: linkExternal }}
+				target="_blank"
+				className={buttonClasses}
+				onMouseEnter={handleButtonMouseEnter}
+				onMouseLeave={handleButtonMouseLeave}
+				ref={buttonRef}
+			>
+				<div className={styles.buttonBox} ref={buttonBoxRef}></div>
+				<span className="relative z-10 block" ref={buttonTextRef}>
+					{text}
+				</span>
 			</Link>
 		);
 	}
-	return <button className={buttonClasses}>{text}</button>;
+	return (
+		<button
+			className={buttonClasses}
+			onMouseEnter={handleButtonMouseEnter}
+			onMouseLeave={handleButtonMouseLeave}
+			ref={buttonRef}
+		>
+			<div className={styles.buttonBox} ref={buttonBoxRef}></div>
+			<span className="relative z-10 block" ref={buttonTextRef}>
+				{text}
+			</span>
+		</button>
+	);
 }
 
 export default Button;
---
+++ client/src/shared/ui/button/model/index.ts
@@ -0,0 +1,50 @@
+import { MutableRefObject } from "react";
+import { gsap } from "gsap";
+
+type Parameters = {
+	buttonBoxRef: MutableRefObject<null>;
+	buttonTextRef: MutableRefObject<null>;
+	buttonRef: MutableRefObject<null>;
+};
+
+function mouseEnterHoverEffect({ buttonBoxRef, buttonTextRef, buttonRef }: Parameters) {
+	gsap.fromTo(
+		buttonBoxRef.current,
+		{ translateY: "100%" },
+		{ translateY: "0%", duration: 0.4, ease: "power2.out" },
+	);
+
+	gsap.fromTo(
+		buttonTextRef.current,
+		{ translateY: "100%", color: "#122947", opacity: 0 },
+		{ translateY: "0%", color: "#122947", opacity: 1, duration: 0.25, ease: "power2.out" },
+	);
+
+	gsap.fromTo(
+		buttonRef.current,
+		{ borderColor: "#b3bac1" },
+		{ borderColor: "white", duration: 0.4, ease: "power2.out" },
+	);
+}
+
+function mouseLeaveHoverEffect({ buttonBoxRef, buttonTextRef, buttonRef }: Parameters) {
+	gsap.fromTo(
+		buttonBoxRef.current,
+		{ translateY: "0%" },
+		{ translateY: "-100%", duration: 0.4, ease: "power2.out" },
+	);
+
+	gsap.fromTo(
+		buttonTextRef.current,
+		{ translateY: "-100%", color: "#122947", opacity: 0 },
+		{ translateY: "0%", color: "#122947", opacity: 1, duration: 0.25, ease: "power2.out" },
+	);
+
+	gsap.fromTo(
+		buttonRef.current,
+		{ borderColor: "white" },
+		{ borderColor: "#b3bac1", duration: 0.4, ease: "power2.out" },
+	);
+}
+
+export { mouseEnterHoverEffect, mouseLeaveHoverEffect };
