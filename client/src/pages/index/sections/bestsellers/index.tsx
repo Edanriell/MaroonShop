d71f13777ed8ba8d5fc3882d9acfb3958848802b
Edanriell <Edanriell@gmail.com>
2023-04-28 23:43:32 +0300

    started work on bestsellers section

--- client/src/pages/index/sections/bestsellers/index.tsx
+++ client/src/pages/index/sections/bestsellers/index.tsx
@@ -1,5 +1,24 @@
-function Bestsellers() {
-	return null;
+import { Button } from "shared/ui";
+
+type Props = {
+	title: string;
+};
+
+function Bestsellers({ title }: Props) {
+	return (
+		<div className="container pt-[6rem] pb-[8rem]">
+			<header className="flex flex-col gap-x-[2.5rem] mb-[5rem]">
+				<h2 className="font-medium font-raleway text-sm-28px text-blue-zodiac-950">
+					{title}
+				</h2>
+				<p className="font-normal font-mPLus text-sm-16px text-blue-zodiac-950">
+					Легендарные продукты, завоевавшие любовь наших клиентов
+				</p>
+			</header>
+			{/* <ProductCard/> */}
+			<Button type="link" text="Смотреть все" link="/" />
+		</div>
+	);
 }
 
 export default Bestsellers;
--- client/src/pages/index/sections/index.tsx
+++ client/src/pages/index/sections/index.tsx
@@ -2,17 +2,20 @@ import { FC, ReactNode } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import HeroSection from "./hero";
+import Bestsellers from "./bestsellers";
 
 type IndexPageSections = Array<{
 	id: string;
 	Section: any;
 	title?: string;
 	srOnlyTitle?: string;
+	classes?: string;
 }>;
 
 type SectionWrapperProps = {
 	srTitle?: string;
 	title?: string;
+	classes?: string;
 	children: ReactNode;
 };
 
@@ -22,10 +25,16 @@ const sections: IndexPageSections = [
 		Section: HeroSection,
 		title: "MAROON",
 	},
+	{
+		id: uuidv4(),
+		Section: Bestsellers,
+		title: "Бестселлеры",
+		classes: "bg-desert-storm-50"
+	},
 ];
 
-const SectionWrapper: FC<SectionWrapperProps> = ({ srTitle, children }) => (
-	<section className="relative z-[1]">
+const SectionWrapper: FC<SectionWrapperProps> = ({ srTitle, children, classes }) => (
+	<section className={classes ?? " " + "relative z-[1]"}>
 		{srTitle && <h2 className="sr-only">{srTitle}</h2>}
 		{children}
 	</section>
@@ -34,8 +43,8 @@ const SectionWrapper: FC<SectionWrapperProps> = ({ srTitle, children }) => (
 const Sections = () => (
 	<main className="flex flex-col">
 		<h1 className="sr-only">Главная страница интернет-магазина Maroon</h1>
-		{sections.map(({ id, Section, srOnlyTitle, title }) => (
-			<SectionWrapper key={id} srTitle={srOnlyTitle}>
+		{sections.map(({ id, Section, srOnlyTitle, title, classes }) => (
+			<SectionWrapper key={id} srTitle={srOnlyTitle} classes={classes}>
 				<Section title={title} />
 			</SectionWrapper>
 		))}
