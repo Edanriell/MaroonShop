981fbc036956d69607ad6b24e6ebbbb83c222fe0
Edanriell <Edanriell@gmail.com>
2023-05-10 22:37:32 +0300

    added some small changes to backend

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/images", express.static(__dirname + "/public/productsImages"));
+app.use("/images", express.static(__dirname + "/public/products-images"));
 
 app.use("/products", productsRouter);
 
